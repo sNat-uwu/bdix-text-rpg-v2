12dffaa54ff82027158ecde9b7be808fd10fded0
Brandon <bdix@hitler.rocks>
2017-06-16 17:46:54 -0700

    first

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main{
+  public static void main(String[] args){
+    Knight player = new Knight("Brandon");
+    System.out.println(player);
+  }
+  public static int chooseMonster(int level){
+    return 0;
+  }
+}
